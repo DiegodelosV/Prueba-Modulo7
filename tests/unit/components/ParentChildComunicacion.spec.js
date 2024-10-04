f58446e8103c7f3da221d78ec7ea4dd28b53251d
DiegodelosV <diegodelosv@gmail.com>
2024-10-04 20:48:32 -0300

    se crea test para la comunicacion entre componentes

---
+++ tests/unit/components/ParentChildComunicacion.spec.js
@@ -0,0 +1,22 @@
+import { mount } from '@vue/test-utils'
+import Parent from '@/components/Parent.vue'
+
+describe('Validar el funcionamiento de la comunicación entre componentes', () => {
+  it('recibe el mensaje del componente Child y lo muestra', async () => {
+    // Arrange: Se monta el componente Parent para la interuacción
+    const wrapper = mount(Parent)
+
+    // Act: ejecutamos la interacción entre los componentes
+    const input = wrapper.get('[data-test="ingresoMensaje"]')
+
+    // Act: Se simula el ingreso de un mensaje en el componente Child
+    await input.setValue('Mensaje de prueba')
+
+    // Act: se prueba el envio del mensaje en el componente Parent
+    const button = wrapper.get('[data-test="Enviar-Mensaje"]')
+    await button.trigger('click') // Se simula el clic con la funcion trigger
+
+    // Assert: Verificamos que el mensaje se haya recibido
+    expect(wrapper.text()).toContain('mensaje recibido: Mensaje de prueba') // Se compara el texto del componente con el texto esperado
+  })
+})
