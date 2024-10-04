87b26ba44a17966a55ab69c7dca109b8a387587e
DiegodelosV <diegodelosv@gmail.com>
2024-10-04 16:43:34 -0300

    se crea el componente y estado contador,

---
+++ src/components/Contador.vue
@@ -0,0 +1,28 @@
+<template>
+  <header>
+    <h1>Contador</h1>
+  </header>
+  <main>
+    <div>
+      <h3>El valor del contador es: {{ contador }}</h3>
+    </div>
+    <div>
+      <button @click="incrementar">Incrementar contador</button>
+      <button @click="decrementar">Disminuir contador</button>
+    </div>
+  </main>
+</template>
+
+<script>
+import { mapState, mapActions } from 'vuex'
+
+export default {
+  name: 'Contador',
+  computed: {
+    ...mapState(['contador'])
+  },
+  methods: {
+    ...mapActions(['incrementar', 'decrementar'])
+  }
+}
+</script>
---
+++ src/views/Ejercicio1View.vue
@@ -0,0 +1,15 @@
+<template>
+  <main>
+    <contador />
+  </main>
+</template>
+
+<script>
+import Contador from '../components/Contador.vue'
+
+export default {
+  components: {
+    Contador
+  }
+}
+</script>
