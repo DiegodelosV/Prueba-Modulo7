539fdc37c82a4cc4344096e9025cad6631a73b46
DiegodelosV <diegodelosv@gmail.com>
2024-10-04 20:47:28 -0300

    se importa librerias bootstrap y fontawesome en el index, modificacion en el nav de app.vue

--- src/App.vue
+++ src/App.vue
@@ -2,9 +2,9 @@
   <header>
     <nav>
       <RouterLink to="/">Home</RouterLink>
-      <RouterLink to="/about">About</RouterLink>
       <RouterLink to="/ejercicio1">Ejercicio 1</RouterLink>
       <RouterLink to="/ejercicio3">Ejercicio 3</RouterLink>
+      <RouterLink to="/about">About</RouterLink>
     </nav>
   </header>
 
