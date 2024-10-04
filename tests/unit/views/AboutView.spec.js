344ce6a1c1ac912c676b8564b18335416fa73ca7
DiegodelosV <diegodelosv@gmail.com>
2024-10-04 18:35:52 -0300

    se realiza test que comprueba la  existencia de componentes que controlen cada una de las vistas.

---
+++ tests/unit/views/AboutView.spec.js
@@ -0,0 +1,28 @@
+import { mount } from '@vue/test-utils'
+import { createRouter, createWebHistory } from 'vue-router'
+
+import AboutView from '@/views/AboutView.vue'
+
+describe('AboutView', () => {
+  test('Probando la existencia del componente AboutView ', async () => {
+    const router = createRouter({
+      history: createWebHistory(),
+      routes: [
+        {
+          path: '/about',
+          name: 'AboutViewVue',
+          component: AboutView
+        }
+      ]
+    })
+    router.push('/about')
+    await router.isReady()
+
+    const wrapper = mount(AboutView, {
+      global: {
+        plugins: [router]
+      }
+    })
+    expect(wrapper.findComponent(AboutView).exists()).toBe(true)
+  })
+})
---
+++ tests/unit/views/HomeView.spec.js
@@ -0,0 +1,30 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import HomeView from '@/views/HomeView.vue'
+import { mount } from '@vue/test-utils'
+
+describe('HomeView', () => {
+  test('Probando la existencia del componente o vista HomeView', async () => {
+    // Crear el router
+    const router = createRouter({
+      history: createWebHistory(),
+      routes: [
+        {
+          path: '/',
+          name: 'home',
+          component: HomeView
+        }
+      ]
+    })
+
+    router.push('/')
+    await router.isReady()
+
+    const wrapper = mount(HomeView, {
+      global: {
+        plugins: [router]
+      }
+    })
+
+    expect(wrapper.findComponent(HomeView).exists()).toBe(true)
+  })
+})
